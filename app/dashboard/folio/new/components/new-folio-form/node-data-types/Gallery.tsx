82e28793d4ade12566ac7642f333741dc848479c
Evandro Viegas <evandrooviegas@gmail.com>
2023-11-14 11:41:16 +0000

    file uploading

--- app/dashboard/folio/new/components/new-folio-form/node-data-types/Gallery.tsx
+++ app/dashboard/folio/new/components/new-folio-form/node-data-types/Gallery.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+"use client" 
+import { Input } from "@/components/ui/input";
+import errorToast from "@/utils/errorToast";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import React, { useState } from "react";
 
 export default function Gallery() {
+  const [] = useState<string[]>([])
+  const supabase = createClientComponentClient()
+  const onFileUpload = async (e: React.FormEvent<HTMLInputElement>) => {
+    const img = e.target?.files?.[0]
+    if(!img) return errorToast()
+    const ext = (img?.name as string).split(".")[1]
+
+    const name = `${crypto.randomUUID()}.${ext}`
+    
+  const { data, error } = await supabase.storage.from("node_images").upload(name, img)
+  console.log(data)
+  console.log(error)
+  }
   return (
-    <div>Gallery</div>
-  )
+    <div>
+      <div>
+        <Input id="picture" type="file" label="Picture" accept="image/png,image/jpeg" onFileUpload={onFileUpload} />
+      </div>
+    </div>
+  );
 }
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,25 +1,55 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  fileType?: "image";
+  onFileUpload?: (e: React.FormEvent<HTMLInputElement>) => void
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md outline-none border border-input bg-background px-3 py-2 text-sm file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+  ({ className, type, label, id, fileType = "image", onFileUpload, ...props }, ref) => {
 
-export { Input }
+    switch (type) {
+      case "file": {
+        return (
+          <div className="px-4 py-1 w-fit rounded text-dimmed border border-input">
+            <label
+              htmlFor={id}
+              className="text-sm"
+            >
+              Choose a file
+            </label>
+            <input
+              id={id}
+              type={type}
+              onInput={onFileUpload}
+              className={cn("hidden", className)}
+              ref={ref}
+              {...props}
+            />
+          </div>
+        );
+      }
+
+      default: {
+        return (
+          <input
+            type={type}
+            className={cn(
+              "flex h-10 w-full rounded-md outline-none border border-input bg-background px-3 py-2 text-sm file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+              className
+            )}
+            ref={ref}
+            {...props}
+          />
+        );
+      }
+    }
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
